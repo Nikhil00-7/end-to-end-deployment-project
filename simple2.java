290d9900d37b5e29ad81f6b97a45bc1a65234bc9
Nikhil <kujurnikhil0007@gmail.com>
2026-02-09 07:28:33 -0800

    new chenges

--- simple2.java
+++ simple2.java
@@ -2,9 +2,9 @@ public class simple2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Test Hello world");
-<<<<<<< HEAD
-=======
+
+   System.out.println("hello again ");
     System.out.println("hello");
->>>>>>> a09ed1e (demo again)
+
     }
 }
